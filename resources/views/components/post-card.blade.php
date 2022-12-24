ba81dd71c6fbe51394c7ac2b1c0c82c703cce8da
sohel-mahmud-avon <sohel@staffasia.org>
2022-12-24 19:21:47 +0600

    post view updated

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -1,3 +1,4 @@
+
 <article
     class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
     <div class="py-6 px-5">
@@ -10,10 +11,8 @@
                 <div class="space-x-2">
                     <a href="#"
                        class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
-                       style="font-size: 10px">Techniques</a>
-                    <a href="#"
-                       class="px-3 py-1 border border-red-300 rounded-full text-red-300 text-xs uppercase font-semibold"
-                       style="font-size: 10px">Updates</a>
+                       style="font-size: 10px">{{ $post }}</a>
+
                 </div>
 
                 <div class="mt-4">
